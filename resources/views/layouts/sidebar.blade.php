5b220768c49be015119ae4c65d9b5e1f87e3f635
ATTOLOU <regisattolou21@outlook.fr>
2025-01-13 14:48:18 +0100

    Création des fichiers list, add, edit, delete dans le dossier subject ainsi que sa migration, son model, controller et ses routes...

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -28,11 +28,13 @@
                    class="flex items-center p-2 text-white rounded-md dark:text-white hover:bg-white dark:hover:bg-violet-600 group transition-all duration-300 ease-out  {{ Request::Segment(2) == 'admin' ? 'bg-white' : '' }}">
                     <svg
                         class="w-5 h-5 transition duration-75 dark:text-white group-hover:text-violet-600 dark:group-hover:text-violet-600 {{ Request::Segment(2) == 'admin' ? 'text-violet-500' : 'text-white' }}"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                        viewBox="0 0 18 18">
-                        <path
-                            d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                              d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
+                              clip-rule="evenodd"/>
                     </svg>
+
                     <span
                         class="ms-3 group-hover:text-violet-600 dark:group-hover:text-violet-600 {{ Request::Segment(2) == 'admin' ? 'text-violet-500' : '' }}">Administrateurs</span>
                 </a>
@@ -50,6 +52,23 @@
                     </svg>
                     <span
                         class="ms-3 group-hover:text-violet-600 dark:group-hover:text-violet-600 {{ Request::Segment(2) == 'class' ? 'text-violet-500' : '' }}">Classes</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ url('admin/subject/list') }}"
+                   class="flex items-center p-2 text-white rounded-md dark:text-white hover:bg-white dark:hover:bg-violet-600 group transition-all duration-300 ease-out  {{ Request::Segment(2) == 'subject' ? 'bg-white' : '' }}">
+                    <svg
+                        class="w-5 h-5 transition duration-75 dark:text-white group-hover:text-violet-600 dark:group-hover:text-violet-600 {{ Request::Segment(2) == 'subject' ? 'text-violet-500' : 'text-white' }}"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                              d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+                              clip-rule="evenodd"/>
+                    </svg>
+
+                    <span
+                        class="ms-3 group-hover:text-violet-600 dark:group-hover:text-violet-600 {{ Request::Segment(2) == 'subject' ? 'text-violet-500' : '' }}">Subjects</span>
                 </a>
             </li>
             <li>
