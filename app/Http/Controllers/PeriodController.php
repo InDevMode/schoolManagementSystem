cc89fd1c3784e2689e0a75eb651aed0a9b3b4db7
regisatl <regisattolou21@outlook.fr>
2025-09-22 19:02:37 +0200

    Implémentation des fonctions update et delete pour une période...

--- app/Http/Controllers/PeriodController.php
+++ app/Http/Controllers/PeriodController.php
@@ -11,15 +11,16 @@ class PeriodController extends Controller
 {
     public function list()
     {
-        $data['header_title'] = 'Liste des Periodes';
+        $data['header_title'] = 'Liste des Périodes';
         $data['getPeriods'] = PeriodModel::getPeriods(5);
         return view('admin.examinations.period.list', $data);
     }
+
     public function add()
     {
-        $data['header_title'] = 'Ajouter une Periode';
-         $getSettings = SettingModel::getSingle(1);
-         $data['getSettings'] = $getSettings ?: null;
+        $data['header_title'] = 'Ajouter une Période';
+        $getSettings = SettingModel::getSingle(1);
+        $data['getSettings'] = $getSettings ?: null;
         return view('admin.examinations.period.add', $data);
     }
     public function create(Request $request)
@@ -34,31 +35,56 @@ class PeriodController extends Controller
             $period->created_by = auth()->user()->id;
             $period->save();
 
-            return redirect('admin/examinations/period/list')->with('success', 'Periode créée avec succès.');
+            return redirect('admin/examinations/period/list')->with('success', 'Période créée avec succès.');
 
         } catch (\Exception $e) {
-            Log::error("Erreur lors de la création d'une periode : " . $e->getMessage());
+            Log::error("Erreur lors de la création d'une période : " . $e->getMessage());
 
             return redirect()->back()->with('error', 'Vos informations ne sont pas correctes. Veuillez réessayer.');
         }
     }
+
     public function edit($id)
     {
         $data['header_title'] = 'Editer une Periode';
         $data['getPeriod'] = PeriodModel::getSingle($id);
+        $getSettings = SettingModel::getSingle(1);
+        $data['getSettings'] = $getSettings ?: null;
         return view('admin.examinations.period.edit', $data);
     }
-    public function update($id)
+
+    public function update(Request $request, $id)
     {
         try {
 
+            $existingPeriod = PeriodModel::getSingle($id);
+            $existingPeriod->name = trim($request->name);
+            $existingPeriod->start_date = $request->start_date;
+            $existingPeriod->end_date = $request->end_date;
+            $existingPeriod->status = intval($request->status);
+            $existingPeriod->settings_id = intval($request->settings_id);
+            $existingPeriod->save();
+
+            return redirect('admin/examinations/period/list')->with('success', 'Période créée avec succès.');
+
         } catch (\Exception $e) {
-            Log::error("Erreur lors de la modification d'une periode : " . $e->getMessage());
+            Log::error("Erreur lors de la modification d'une période : " . $e->getMessage());
 
             return redirect()->back()->with('error', 'Vos informations ne sont pas correctes. Veuillez réessayer.');
         }
     }
     public function delete($id)
     {
+        $period = PeriodModel::getSingle($id);
+        if ($period) {
+            $period->is_delete = 1;
+            $period->save();
+            return redirect()->back()->with('success', 'Période supprimé avec succès.');
+        } else {
+            abort(404);
+        }
     }
+
+
+
 }
